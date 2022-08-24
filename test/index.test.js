46391c5deaaa958112694f9b4dec3b4a338c0729
PedroBrandao05 <pedro-brandao2012@hotmail.com>
2022-08-24 09:30:23 -0300

    teste 2

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,14 @@
 const catchFiles = require('../index');
 
-test('Must be a function', () => {
-    expect(typeof catchFiles).toBe('function')
-});
+const arrayResult = [
+    {
+       FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+    }
+] 
 
+describe('catchFiles::', () => {
+    it('Must return a result array', async () => {
+        const result = await catchFiles('C:\/Users\/userlocal\/Downloads\/2299-lib-nodejs-markdown-aula-5\/2299-lib-nodejs-markdown-aula-5\/test\/arquivos_teste\/texto1teste.md');
+        expect(result).toEqual(arrayResult);
+    })
+})
